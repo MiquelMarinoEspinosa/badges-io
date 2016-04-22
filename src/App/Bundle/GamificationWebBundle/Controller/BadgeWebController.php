0c5812dd6f38bc5f62e4024802cb2cf1d25dfb78
miquel.marino <miquel.marino@atrapalo.com>
2016-04-22 17:15:49 +0200

    First approach ListBadges web version

--- src/App/Bundle/GamificationWebBundle/Controller/BadgeWebController.php
+++ src/App/Bundle/GamificationWebBundle/Controller/BadgeWebController.php
@@ -2,12 +2,79 @@
 
 namespace App\Bundle\GamificationWebBundle\Controller;
 
+use Infrastructure\Resource\Domain\Entity\Badge\BadgeResource;
+use Interactor\CommandHandler\ListBadges\ListBadgesCommand;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BadgeWebController extends Controller
 {
-    public function indexAction($userId)
+    const IS_MULTI_USER = 1;
+
+    public function listBadgesAction($userId)
     {
-        return $this->render('GamificationWebBundle::index.html.twig', ['userId' => $userId]);
+        $listBadgesCommand = $this->buildListBadgesCommandByRequest($userId);
+
+        $badgesResources = $this->container->get(
+            "gamification.interactor.command_handler.list_badges.list_badges_command_handler"
+        )->handle($listBadgesCommand);
+
+        return $this->renderHtmlResult($userId, $badgesResources);
+    }
+
+    /**
+     * @param string $userId
+     *
+     * @return ListBadgesCommand
+     */
+    private function buildListBadgesCommandByRequest($userId)
+    {
+        return new ListBadgesCommand($userId);
+    }
+
+    /**
+     * @param string $userId
+     * @param BadgeResource[] $badgesResources
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    private function renderHtmlResult($userId, $badgesResources)
+    {
+        return $this->render(
+            'GamificationWebBundle::badges.list.html.twig',
+            [
+                'userId' => $userId,
+                'badges' => $this->buildBadgesResourcesAsArray($badgesResources)
+            ]
+        );
+    }
+
+    /**
+     * @param BadgeResource[] $badgesResources
+     *
+     * @return array
+     */
+    private function buildBadgesResourcesAsArray($badgesResources)
+    {
+        $badgesResourcesAsArray = [];
+        foreach ($badgesResources as $badgeResource) {
+            $badgesResourcesAsArray[] = $this->buildBadgeResourceAsArray($badgeResource);
+        }
+
+        return $badgesResourcesAsArray;
+    }
+
+    /**
+     * @param BadgeResource $badgeResource
+     *
+     * @return array
+     */
+    private function buildBadgeResourceAsArray(BadgeResource $badgeResource)
+    {
+        return [
+            'name'          => $badgeResource->name(),
+            'description'   => $badgeResource->description(),
+            'isMultiUser'   => ($badgeResource->isMultiUser() == static::IS_MULTI_USER) ? 'Yes' : 'No',
+            'imageHref'     => $badgeResource->imageResource()->href()
+        ];
     }
 }
